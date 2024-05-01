07564122459a7c328c2e0caf157283d2f3fcee18
vossr <pehkonenroni@gmail.com>
2024-05-01 19:01:49 +0300

    implemented render pass for the new shader

--- hologram_rendering.py
+++ hologram_rendering.py
@@ -7,9 +7,6 @@ import cv2
 
 device = bridge_api.get_device(0)
 
-# vert = "#version 330 core\n" + device.shader.vertex_shader
-# frag = "#version 330 core\n" + device.shader.fragment_shader
-
 if not glfw.init():
     raise Exception("Failed to initialize GLFW")
 
@@ -35,57 +32,30 @@ glfw.make_context_current(window)
 quad_VAO = gl_utils.setup_quad_vertices()
 quad_shader = gl_utils.create_shader_program(open('quad_texture_vert.glsl', 'r').read(), open('quad_texture_frag.glsl', 'r').read())
 quilt_VAO = gl_utils.setup_quilt_vertices()
-quilt_shader = gl_utils.create_shader_program(open('look_old_vert.glsl', 'r').read(), open('look_old_frag.glsl', 'r').read())
-# quilt_shader = gl_utils.create_shader_program(open('look_vert.glsl', 'r').read(), open('look_frag.glsl', 'r').read())
+quilt_shader_old = 0
+# quilt_shader_old = gl_utils.create_shader_program(open('look_old_vert.glsl', 'r').read(), open('look_old_frag.glsl', 'r').read())
+
+vert = "#version 330 core\n" + device.shader.vertex_shader
+frag = "#version 330 core\n" + device.shader.fragment_shader
+quilt_shader = gl_utils.create_shader_program(vert, frag)
 
 
-
-
-
-def render_pass_quilt(texture_id):
+def render_pass_quilt_old_webgl_shader(texture_id):
     glClear(GL_COLOR_BUFFER_BIT)
-    glUseProgram(quilt_shader)
+    glUseProgram(quilt_shader_old)
 
-    unused_uniforms = """
-    glUniform1f(glGetUniformLocation(quilt_shader, "pitch"), device.shader.lenticular_pitch)
-    glUniform1f(glGetUniformLocation(quilt_shader, "tilt"), device.shader.lenticular_tilt)
-    glUniform1f(glGetUniformLocation(quilt_shader, "center"), device.shader.center_offset)
-    glUniform1f(glGetUniformLocation(quilt_shader, "subp"), device.shader.subpixel_size)
-    glUniform1f(glGetUniformLocation(quilt_shader, "displayAspect"), device.window.aspect_ratio)
-    glUniform1f(glGetUniformLocation(quilt_shader, "quiltAspect"), device.quilt.tiling_dimension_x / device.quilt.tiling_dimension_y)
-    # glUniform1f(glGetUniformLocation(quilt_shader, "quiltAspect"), 1.0)
-    
-    glUniform1i(glGetUniformLocation(quilt_shader, "invView"), device.shader.should_invert)
-    glUniform1i(glGetUniformLocation(quilt_shader, "ri"), device.shader.red_index)
-    glUniform1i(glGetUniformLocation(quilt_shader, "bi"), device.shader.blue_index)
-    glUniform1i(glGetUniformLocation(quilt_shader, "overscan"), 1)
-    glUniform1i(glGetUniformLocation(quilt_shader, "quiltInvert"), 0)
-    glUniform1i(glGetUniformLocation(quilt_shader, "debug"), 0)
-
+    glUniform1f(glGetUniformLocation(quilt_shader_old, "pitch"), device.shader.lenticular_pitch)
+    glUniform1f(glGetUniformLocation(quilt_shader_old, "tilt"), device.shader.lenticular_tilt)
+    glUniform1f(glGetUniformLocation(quilt_shader_old, "center"), device.shader.center_offset)
+    glUniform1f(glGetUniformLocation(quilt_shader_old, "invView"), device.shader.should_invert)
+    glUniform1f(glGetUniformLocation(quilt_shader_old, "subp"), device.shader.subpixel_size)
     quilt_image_count = device.quilt.tiling_dimension_x * device.quilt.tiling_dimension_y
-    glUniform3fv(glGetUniformLocation(quilt_shader, "tile"), 1, (device.quilt.tiling_dimension_x, device.quilt.tiling_dimension_y, quilt_image_count))
-    tileWidth = round(device.window.w / device.quilt.tiling_dimension_x)
-    tileHeight = round(device.window.y / device.quilt.tiling_dimension_y)
-    quiltViewPortion = (
-        device.quilt.tiling_dimension_x * tileWidth * device.window.w,
-        device.quilt.tiling_dimension_y * tileHeight * device.window.h
-    )
-    glUniform2fv(glGetUniformLocation(quilt_shader, "viewPortion"), 1, quiltViewPortion)
-    # glUniform1i(glGetUniformLocation(quilt_shader, "screenTex"), texture_id)
-    """
-
-    glUniform1f(glGetUniformLocation(quilt_shader, "pitch"), device.shader.lenticular_pitch)
-    glUniform1f(glGetUniformLocation(quilt_shader, "tilt"), device.shader.lenticular_tilt)
-    glUniform1f(glGetUniformLocation(quilt_shader, "center"), device.shader.center_offset)
-    glUniform1f(glGetUniformLocation(quilt_shader, "invView"), device.shader.should_invert)
-    glUniform1f(glGetUniformLocation(quilt_shader, "subp"), device.shader.subpixel_size)
-    quilt_image_count = device.quilt.tiling_dimension_x * device.quilt.tiling_dimension_y
-    glUniform1f(glGetUniformLocation(quilt_shader, "numViews"), quilt_image_count)
-    glUniform1f(glGetUniformLocation(quilt_shader, "tilesX"), device.quilt.tiling_dimension_x)
-    glUniform1f(glGetUniformLocation(quilt_shader, "tilesY"), device.quilt.tiling_dimension_y)
+    glUniform1f(glGetUniformLocation(quilt_shader_old, "numViews"), quilt_image_count)
+    glUniform1f(glGetUniformLocation(quilt_shader_old, "tilesX"), device.quilt.tiling_dimension_x)
+    glUniform1f(glGetUniformLocation(quilt_shader_old, "tilesY"), device.quilt.tiling_dimension_y)
 
     tileWidth = round(device.window.w / device.quilt.tiling_dimension_x)
-    tileHeight = round(device.window.y / device.quilt.tiling_dimension_y)
+    tileHeight = round(device.window.h / device.quilt.tiling_dimension_y)
     quiltViewPortion = (
         device.quilt.tiling_dimension_x * tileWidth * device.window.w,
         device.quilt.tiling_dimension_y * tileHeight * device.window.h
@@ -93,8 +63,55 @@ def render_pass_quilt(texture_id):
     quiltViewPortion = (
         1.0, 1.0
     )
-    glUniform2fv(glGetUniformLocation(quilt_shader, "quiltViewPortion"), 1, quiltViewPortion)
-    # glUniform1i(glGetUniformLocation(quilt_shader, "u_texture"), texture_id)
+    glUniform2fv(glGetUniformLocation(quilt_shader_old, "quiltViewPortion"), 1, quiltViewPortion)
+    # glUniform1i(glGetUniformLocation(quilt_shader_old, "u_texture"), texture_id)
+
+    glActiveTexture(GL_TEXTURE0)
+    glBindTexture(GL_TEXTURE_2D, texture_id)
+    glBindVertexArray(quilt_VAO)
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4)
+
+    glfw.swap_buffers(window)
+    glfw.poll_events()
+
+
+def render_pass_quilt(texture_id):
+    glClear(GL_COLOR_BUFFER_BIT)
+    glUseProgram(quilt_shader)
+
+    glUniform1f(glGetUniformLocation(quilt_shader, "pitch"), device.shader.lenticular_pitch)
+    glUniform1f(glGetUniformLocation(quilt_shader, "tilt"), device.shader.lenticular_tilt)
+    glUniform1f(glGetUniformLocation(quilt_shader, "center"), device.shader.center_offset)
+    glUniform1i(glGetUniformLocation(quilt_shader, "invView"), device.shader.should_invert)
+    glUniform1f(glGetUniformLocation(quilt_shader, "subp"), device.shader.subpixel_size)
+    glUniform1f(glGetUniformLocation(quilt_shader, "displayAspect"), device.window.aspect_ratio)
+
+    #why this in wrong order h / w
+    glUniform1f(glGetUniformLocation(quilt_shader, "quiltAspect"), device.quilt.tiling_dimension_y / device.quilt.tiling_dimension_x)
+    # glUniform1f(glGetUniformLocation(quilt_shader, "quiltAspect"), device.quilt.tiling_dimension_x / device.quilt.tiling_dimension_y)
+    # glUniform1f(glGetUniformLocation(quilt_shader, "quiltAspect"), 1.0)
+
+    glUniform1i(glGetUniformLocation(quilt_shader, "ri"), device.shader.red_index)
+    glUniform1i(glGetUniformLocation(quilt_shader, "bi"), device.shader.blue_index)
+
+
+
+    quilt_image_count = device.quilt.tiling_dimension_x * device.quilt.tiling_dimension_y
+    glUniform3fv(glGetUniformLocation(quilt_shader, "tile"), 1, (device.quilt.tiling_dimension_x, device.quilt.tiling_dimension_y, quilt_image_count))
+
+    tileWidth = round(device.window.w / device.quilt.tiling_dimension_x)
+    tileHeight = round(device.window.h / device.quilt.tiling_dimension_y)
+    quiltViewPortion = (
+        device.quilt.tiling_dimension_x * tileWidth * device.window.w,
+        device.quilt.tiling_dimension_y * tileHeight * device.window.h
+    )
+    quiltViewPortion = (
+        1.0, 1.0
+    )
+    glUniform2fv(glGetUniformLocation(quilt_shader, "viewPortion"), 1, quiltViewPortion)
+    glUniform1i(glGetUniformLocation(quilt_shader, "overscan"), 0)
+    glUniform1i(glGetUniformLocation(quilt_shader, "quiltInvert"), 0)
+    glUniform1i(glGetUniformLocation(quilt_shader, "debug"), 0)
 
 
     glActiveTexture(GL_TEXTURE0)
@@ -132,8 +149,7 @@ def _generate_quilt_from_rgbd(rgb_depth):
 
 
 
-
-def show_lenticular_projection(lent):
+def show_image(lent):
     texture_id = gl_utils.load_texture_from_cv_image(lent)
     render_pass_quad(texture_id)
     glDeleteTextures(1, [texture_id])
@@ -144,6 +160,7 @@ def show_quilt(quilt):
 
     texture_id = gl_utils.load_texture_from_cv_image(quilt)
     render_pass_quilt(texture_id)
+    # render_pass_quilt_old_webgl_shader(texture_id)
     glDeleteTextures(1, [texture_id])
     # lent = _generate_lenticular_projection(quilt)
     # show_lenticular_projection(lent)
