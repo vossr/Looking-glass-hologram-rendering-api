19f2f3179a6f5f77e71ba2271a20bf8a94b04721
vossr <pehkonenroni@gmail.com>
2024-05-05 13:02:21 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hologram_rendering',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    author='rpehkone',
+    description='An API for bridge interactions',
+    license='CC0',
+    keywords='looking glass hologram rendering api',
+    url='https://github.com/rpehkone/looking-glass-rendering-python-api'
+)
